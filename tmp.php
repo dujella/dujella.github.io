fbd9085d4277e6d81392b9720812c23a87bab8d2
dujella <71018251+dujella@users.noreply.github.com>
2020-09-09 17:10:34 +0200

    Add files via upload

---
+++ tmp.php
@@ -0,0 +1,10 @@
+<?php 
+$b = time (); 
+print date("m/d/y",$b) . "<br>"; 
+print date("D, F jS",$b) . "<br>"; 
+print date("l, F jS Y",$b) . "<br>"; 
+print date("g:i A",$b) . "<br>"; 
+print date("r",$b) . "<br>"; 
+print date("g:i:s A D, F jS Y",$b) . "<br>"; 
+print date("d M Y, D, H:i:s",$b) . "<br>";
+?> 
